508c470db8899ca72deccb23e014cc15427381d0
gimyojun <gywnsdl5623@gmail.com>
2023-10-30 13:19:54 +0900

    빼기 테스트

--- src/main/java/com/ll/Calc.java
+++ src/main/java/com/ll/Calc.java
@@ -6,13 +6,19 @@ public class Calc {
 
 
     public static int run(String exp){
-            if(exp.contains("-")){
-                return 0;
-            }else if(exp.contains("+")){
-                return 20;
-            }
+        if(exp.isEmpty())return 0;
 
-        return 0;
+        final boolean isPlus = exp.contains("+");
+
+        final String divideSign = isPlus ? "\\+" : "-";
+
+        final String[] expBits = exp.split(" " + divideSign + " ");
+
+        final int num1 = Integer.parseInt(expBits[0]);
+        final int num2 = Integer.parseInt(expBits[1]);
+
+        if (isPlus) return num1 + num2;
+        else return num1 - num2;
     }
 
 
--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -4,7 +4,12 @@ package com.ll;
 public class Main {
     public static void main(String[] args) {
 
+        String input = "3+5+7-2";
+        String[] parts = input.split("-");
 
+        for (String part : parts) {
+            System.out.println(part);
+        }
 
     }
 }
